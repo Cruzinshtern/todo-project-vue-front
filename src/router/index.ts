fc240ec28f06a930df2467ca995a9a009d4349b4
Bohdan <bohdanmatv1@gmail.com>
2025-04-12 15:16:34 +0300

    basic view layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CreateTodoView from '@/views/CreateTodoView.vue'
+import EditTodoView from '@/views/EditTodoView.vue'
+import SettingsView from '@/views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/create-todo',
+      name: 'createTodo',
+      component: CreateTodoView,
+    },
+    {
+      path: '/edit-todo',
+      name: 'editTodo',
+      component: EditTodoView,
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
     },
   ],
 })
---
+++ src/views/EditTodoView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Edit Todo view</div>
+</template>
